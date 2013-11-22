4a4de792dee3217688bc721c2ac5e003994da71a
Rajesh Rajendran <rajesh.r@columbusagain.com>
2013-11-22 13:08:20 +0530

    Ui changes.....

---
+++ src/com/columbusagain/camark/Rollno.java
@@ -0,0 +1,49 @@
+package com.columbusagain.camark;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Rollno extends Activity {
+	EditText rollno;
+	Button submit;
+	Activity mainactivity;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_rollno);
+		rollno = (EditText) findViewById(R.id.rollnotext);
+		submit = (Button) findViewById(R.id.rollnobtn);
+		submit.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (rollno.getText().toString().compareTo("") == 0) {
+					Toast.makeText(mainactivity, "Please enter a roll number",
+							Toast.LENGTH_SHORT).show();
+				} else {
+					Intent intent = new Intent(Rollno.this, MainActivity.class);
+					intent.putExtra("rollno", rollno.getText().toString());
+					startActivity(intent);
+
+				}
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.rollno, menu);
+		return true;
+	}
+
+}
